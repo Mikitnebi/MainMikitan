37a7d582d79993ffdadc5a3ea96c96b51a6792c2
Lazarus88 <lpach20@freeuni.edu.ge>
2023-07-24 01:43:38 +0400

    Add Customer

---
+++ MainMikitani.Domain/Models/Customer/CustomerAnalytics.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMikitani.Domain.Models.Customer
+{
+    public class CustomerAnalytics
+    {
+        public int TotalOrderQuantity { get; set; }
+        public int AvarageMonthlyOrderQuantity { get; set; }
+        public int TotalOfProfit { get; set; }
+        public int AvarageMonthlyProfit { get; set; }
+    }
+}
