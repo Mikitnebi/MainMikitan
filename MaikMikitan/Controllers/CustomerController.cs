100ab6979ddc7c79b8e92cf8a335e22d3aab9153
SpinningWheel <jabachkheidze@gmail.com>
2023-07-23 23:11:48 +0400

    added customerController

---
+++ MaikMikitan/Controllers/CustomerController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MainMikitan.API.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+       
+    public class CustomerController : ControllerBase {
+
+    }
+}
