c4e48a177803544e372a05db1b807b41e656f107
ruthless <jdundua@gmail.com>
2023-10-07 17:38:05 +0400

    opt

--- MainMikitan.Database/Features/Common/Multifunctional/Repository/MultifunctionalRepository.cs
+++ MainMikitan.Database/Features/Common/Multifunctional/Repository/MultifunctionalRepository.cs
@@ -2,80 +2,53 @@ using System.Data.SqlClient;
 using Dapper;
 using System.Data;
 using System.Reflection;
+using MainMikitan.Database.Features.Common.Multifunctional.Interface.Query;
+using MainMikitan.Database.Features.Common.Multifunctional.Interface.Repository;
+using MainMikitan.Domain.Models.Setting;
 
 namespace MainMikitan.Database.Features.Common.Multifunctional.Repository;
 
-public class MultifunctionalRepository
+public class MultifunctionalRepository : IMultifunctionalRepository
 {
-    private readonly IDbConnection _dbConnection;
+    private readonly ConnectionStringsOptions _connectionString;
+    private readonly IMultifunctionalQuery _multifunctionalQuery;
 
-    public MultifunctionalRepository(string connectionString)
+    public MultifunctionalRepository(
+        IMultifunctionalQuery multifunctionalQuery, 
+        ConnectionStringsOptions connectionString)
     {
-        _dbConnection = new SqlConnection(connectionString);
+        _multifunctionalQuery = multifunctionalQuery;
+        _connectionString = connectionString;
     }
 
     public async Task AddOrUpdateTableData<T>(T model) where T : class
     {
-        if (typeof(T).BaseType is null || typeof(T).BaseType.Name != "MultifunctionalQueryMainModel")
+        using IDbConnection connection = new SqlConnection(_connectionString.MainMik);
+        
+        if (typeof(T).BaseType is null || typeof(T).BaseType?.Name != "MultifunctionalQueryMainModel")
         {
             throw new Exception("Class must be child of MultifunctionalQueryMainModel");
         }
         
         var properties =
             typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-
+        
         var databaseName = properties.FirstOrDefault(p => p.Name == "DatabaseName");
         var schemaName = properties.FirstOrDefault(p => p.Name == "SchemaName");
         var tableName = $"[{databaseName}].[{schemaName}].[{typeof(T).Name}]";
-        
         var id = properties.FirstOrDefault(i => i.Name == "Id");
+        
         if (id is null)
         {
-            var createSql = GenerateCreateQuery(properties, tableName);
+            var createSql = _multifunctionalQuery.GenerateCreateQuery(properties, tableName);
             
-            await _dbConnection.ExecuteAsync(createSql, model);
+            await connection.ExecuteAsync(createSql, model);
             
             return;
         }
 
-        var updateQuery = GenerateUpdateQuery(properties, tableName);
+        var updateQuery = _multifunctionalQuery.GenerateUpdateQuery(properties, tableName);
 
-        await _dbConnection.ExecuteAsync(updateQuery, model);
-    }
-
-    private string GenerateCreateQuery(PropertyInfo[] properties, string tableName)
-    {
-        var createSql = $"INSERT INTO {tableName} VALUES(";
-            
-        foreach (var property in properties)
-        {
-            if (property.Name is not ("Id" and "DatabaseName" and "SchemaName"))
-            {
-                createSql += $"@{property.Name}, ";
-            }
-        }
-            
-        createSql = createSql.TrimEnd(',', ')');
-
-        return createSql;
-    }
-
-    private string GenerateUpdateQuery(PropertyInfo[] properties, string tableName)
-    {
-        var updateSql = $"UPDATE {tableName} SET ";
-
-        foreach (var property in properties)
-        {
-            if (property.Name is not ("Id" and "DatabaseName" and "SchemaName"))
-            {
-                updateSql += $"{property.Name} = @{property.Name}, ";
-            }
-        }
-
-        updateSql = updateSql.TrimEnd(',', ' ');
-
-        updateSql += " WHERE Id = @Id";
-
-        return updateSql;
+        await connection.ExecuteAsync(updateQuery, model);
     }
 }
