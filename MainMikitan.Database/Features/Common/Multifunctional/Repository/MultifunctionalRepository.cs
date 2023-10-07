34d65d5cc1bd0cbb10d15a76c2547c0d5ee7b1d2
ruthless <jdundua@gmail.com>
2023-10-07 19:22:55 +0400

    create table added

--- MainMikitan.Database/Features/Common/Multifunctional/Repository/MultifunctionalRepository.cs
+++ MainMikitan.Database/Features/Common/Multifunctional/Repository/MultifunctionalRepository.cs
@@ -6,6 +6,7 @@ using MainMikitan.Database.Features.Common.Multifunctional.Interface.Query;
 using MainMikitan.Database.Features.Common.Multifunctional.Interface.Repository;
 using MainMikitan.Domain.Models.Setting;
 using MainMikitan.InternalServiceAdapterService.Exceptions;
+using NPOI.SS.Formula.Functions;
 
 namespace MainMikitan.Database.Features.Common.Multifunctional.Repository;
 
@@ -51,5 +52,41 @@ public class MultifunctionalRepository : IMultifunctionalRepository
         var updateQuery = _multifunctionalQuery.GenerateUpdateQuery(properties, tableName);
 
         await connection.ExecuteAsync(updateQuery, model);
+    }
+
+    public async Task CreateOrUpdateTable<T>(List<T> model) where T : class
+    {
+        using IDbConnection connection = new SqlConnection(_connectionString.MainMik);
+        
+        if (typeof(T).BaseType is null || typeof(T).BaseType?.Name != "MultifunctionalQueryMainModel")
+        {
+            throw new MainMikitanException($"{typeof(T)} Class must be child of MultifunctionalQueryMainModel", "CreateOrUpdateTable");
+        }
+        var tableExistsQuery = _multifunctionalQuery.TableExistsQuery();
+        
+        var properties =
+            typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+        
+        var databaseName = properties.FirstOrDefault(p => p.Name == "DatabaseName");
+        var schemaName = properties.FirstOrDefault(p => p.Name == "SchemaName");
+        var tableName = typeof(T).Name;
+        
+        var tableExists = await connection.QueryFirstOrDefaultAsync<bool>(tableExistsQuery, new
+        {
+            schema = schemaName,
+            table = tableName,
+            database = databaseName
+        });
+
+        if (tableExists)
+        {
+            throw new MainMikitanException("Table Already Exists", "CreateOrUpdateTable");
+        }
+
+        tableName = $"[{databaseName}].[{schemaName}].[{tableName}]";
+
+        var tableCreateQuery = _multifunctionalQuery.GenerateCreateTableQuery(tableName, properties);
+        
+        await connection.ExecuteAsync(tableCreateQuery, model);
     }
 }
