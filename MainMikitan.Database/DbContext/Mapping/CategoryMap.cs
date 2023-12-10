d0f56b4a6942670a4d9cf44d10f0eff70c1323bd
RuthLess1303 <63645937+RuthLess1303@users.noreply.github.com>
2023-12-10 17:35:02 +0400

    Category Table added

---
+++ MainMikitan.Database/DbContext/Mapping/CategoryMap.cs
@@ -0,0 +1,18 @@
+﻿using MainMikitan.Domain.Models.Common;
+using MainMikitan.Domain.Models.Customer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MainMikitan.Database.DbContext.Mapping;
+
+public class CategoryMap : IEntityTypeConfiguration<CategoryEntity>
+{
+    public void Configure(EntityTypeBuilder<CategoryEntity> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.StatusId).IsRequired();
+        builder.Property(b => b.NameEng).IsRequired();
+        builder.Property(b => b.NameGeo).IsRequired();
+        builder.Property(b => b.CreatedAt).IsRequired();
+    }
+}
---
+++ MainMikitani.Domain/Models/Common/CategoryEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainMikitan.Domain.Models.Common;
+
+public class CategoryEntity
+{
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public int StatusId { get; set; }
+    [Required]
+    public string NameGeo { get; set; } = null!;
+    [Required]
+    public string NameEng { get; set; } = null!;
+    [Required]
+    public DateTime CreatedAt { get; set; }
+}
