3a20b90e8b6ec200cf98199fb72b6fe027887296
RuthLess1303 <63645937+RuthLess1303@users.noreply.github.com>
2024-01-23 21:00:20 +0400

    გამოუყენებელი რეფერენსების ამოშლა

--- MaikMikitan/Controllers/RestaurantController.cs
+++ MaikMikitan/Controllers/RestaurantController.cs
@@ -1,5 +1,4 @@
-﻿using FluentEmail.Core;
-using MainMikitan.API.Extentions;
+﻿using MainMikitan.API.Extentions;
 using MainMikitan.API.Filters;
 using MainMikitan.Application.Features.Customer.Commands;
 using MainMikitan.Application.Features.Restaurant.Registration.Commands;
@@ -8,7 +7,6 @@ using MainMikitan.Domain.Requests.RestaurantRequests;
 using MediatR;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using static MainMikitan.Domain.Enums;
 
 namespace MainMikitan.API.Controllers {
