1487f4a8511ac9fd5fd5f480bab2332668a12f66
RuthLess1303 <63645937+RuthLess1303@users.noreply.github.com>
2024-03-03 03:05:38 +0400

    Migration added

---
+++ MainMikitan.Database/Migrations/20240302230504_EventDetailsEngsAdded.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MainMikitan.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventDetailsEngsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "EventDetails",
+                newName: "NameGeo");
+
+            migrationBuilder.RenameColumn(
+                name: "EventAddress",
+                table: "EventDetails",
+                newName: "EventAddressGeo");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "EventDetails",
+                newName: "NameEng");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionEng",
+                table: "EventDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionGeo",
+                table: "EventDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EventAddressEng",
+                table: "EventDetails",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescriptionEng",
+                table: "EventDetails");
+
+            migrationBuilder.DropColumn(
+                name: "DescriptionGeo",
+                table: "EventDetails");
+
+            migrationBuilder.DropColumn(
+                name: "EventAddressEng",
+                table: "EventDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "NameGeo",
+                table: "EventDetails",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "NameEng",
+                table: "EventDetails",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "EventAddressGeo",
+                table: "EventDetails",
+                newName: "EventAddress");
+        }
+    }
+}
